2f8526ae6d4e1f2a1e947f5473b7c9d2c36288a7
Melody Kimball <melody@forestkimball.com>
2024-10-07 14:04:26 -0400

    Updated SKILL_DETAIL to use SKILL_OWNER

--- src/ETL/SKILL_DETAIL.sql
+++ src/ETL/SKILL_DETAIL.sql
@@ -2,102 +2,100 @@ TRUNCATE TABLE SKILL_DETAIL;
 
 INSERT INTO SKILL_DETAIL
   (
-    SKILL_DETAIL_PROGRAM_ID,
-    SKILL_DETAIL_PROGRAM_NAME,
-    SKILL_DETAIL_REGISTRY_ID,
+    SKILL_DETAIL_OWNER_ID,
     SKILL_DETAIL_ID,
     SKILL_DETAIL_DESCRIPTION,
     SKILL_DETAIL_PARENT_ID,
+    SKILL_DETAIL_ORGUNIT_ID,
+    SKILL_DETAIL_ORGUNIT_TYPE,
+    SKILL_DETAIL_ORGUNIT_CODE,
+    SKILL_DETAIL_ORGUNIT_NAME,
+    SKILL_DETAIL_PROGRAM_ID,
+    SKILL_DETAIL_PROGRAM_NAME,
     SKILL_DETAIL_SOURCE
   )
 SELECT
-  SKILL_PROGRAM_ID AS SKILL_DETAIL_PROGRAM_ID,
-  SKILL_PROGRAM_NAME AS SKILL_DETAIL_PROGRAM_NAME,
-  SKILL_PROGRAM_REGISTRY_ID AS SKILL_DETAIL_REGISTRY_ID,
-  OutcomeDetail.OutcomeId AS SKILL_DETAIL_ID,
-  OutcomeDetail.Description AS SKILL_DETAIL_DESCRIPTION,
+  SKILL_OWNER_ID                AS SKILL_DETAIL_OWNER_ID,
+  OutcomeDetail.OutcomeId       AS SKILL_DETAIL_ID,
+  OutcomeDetail.Description     AS SKILL_DETAIL_DESCRIPTION,
   OutcomeDetail.ParentOutcomeId AS SKILL_DETAIL_PARENT_ID,
-  'Program' AS SKILL_DETAIL_SOURCE
+  SKILL_OWNER_ORGUNIT_ID        AS SKILL_DETAIL_ORGUNIT_ID,
+  SKILL_OWNER_ORGUNIT_TYPE      AS SKILL_DETAIL_ORGUNIT_TYPE,
+  SKILL_OWNER_ORGUNIT_CODE      AS SKILL_DETAIL_ORGUNIT_CODE,
+  SKILL_OWNER_ORGUNIT_NAME      AS SKILL_OWNER_ORGUNIT_NAME,
+  SKILL_OWNER_PROGRAM_ID        AS SKILL_DETAIL_PROGRAM_ID,
+  SKILL_OWNER_PROGRAM_NAME      AS SKILL_DETAIL_PROGRAM_NAME,
+  CASE
+    WHEN SKILL_OWNER_PROGRAM_ID IS NOT NULL THEN 'Program'
+    ELSE 'Course'
+  END AS SKILL_DETAIL_SOURCE
 FROM
-  SKILL_PROGRAM,
+  SKILL_OWNER SkillOwner,
   D2L_OUTCOMES_IN_REGISTRY OutcomeRegistry,
   D2L_OUTCOME_DETAIL OutcomeDetail
 WHERE
-  OutcomeRegistry.RegistryId = SKILL_PROGRAM_REGISTRY_ID and
-  NVL(OutcomeRegistry.IsDeleted, 0) = 0 and
-  OutcomeDetail.OutcomeId = OutcomeRegistry.OutcomeId and
-  NVL(OutcomeDetail.IsDeleted, 0) = 0
+  OutcomeRegistry.RegistryId = SKILL_OWNER_ID AND
+  OutcomeDetail.OutcomeId = OutcomeRegistry.OutcomeId
+;
+
+MERGE INTO
+  SKILL_DETAIL A
+USING
+  (
+    SELECT
+      Parent.SKILL_DETAIL_PROGRAM_ID,
+      Parent.SKILL_DETAIL_PROGRAM_NAME,
+      Child.SKILL_DETAIL_OWNER_ID,
+      Child.SKILL_DETAIL_ID
+    FROM
+      SKILL_DETAIL Parent,
+      SKILL_DETAIL Child
+    WHERE
+      Parent.SKILL_DETAIL_PROGRAM_ID IS NOT NULL AND
+      Child.SKILL_DETAIL_ID = Parent.SKILL_DETAIL_ID AND
+      Child.SKILL_DETAIL_PROGRAM_ID IS NULL
+  ) B
+ON
+  (
+    A.SKILL_DETAIL_OWNER_ID = B.SKILL_DETAIL_OWNER_ID AND
+    A.SKILL_DETAIL_ID = B.SKILL_DETAIL_ID
+  )
+WHEN MATCHED THEN
+  UPDATE SET
+    A.SKILL_DETAIL_PROGRAM_ID = B.SKILL_DETAIL_PROGRAM_ID,
+    A.SKILL_DETAIL_PROGRAM_NAME = B.SKILL_DETAIL_PROGRAM_NAME
 ;
 
 DECLARE
   record_count NUMBER;
 BEGIN
   LOOP
-    INSERT INTO SKILL_DETAIL
+    MERGE INTO
+      SKILL_DETAIL A
+    USING
       (
-        SKILL_DETAIL_PROGRAM_ID,
-        SKILL_DETAIL_PROGRAM_NAME,
-        SKILL_DETAIL_REGISTRY_ID,
-        SKILL_DETAIL_ID,
-        SKILL_DETAIL_DESCRIPTION,
-        SKILL_DETAIL_PARENT_ID,
-        SKILL_DETAIL_SOURCE
-      )
-    SELECT
-      SKILL_DETAIL_PROGRAM_ID AS SKILL_DETAIL_PROGRAM_ID,
-      SKILL_DETAIL_PROGRAM_NAME AS SKILL_DETAIL_PROGRAM_NAME,
-      OutcomeRegistry.RegistryId AS SKILL_DETAIL_REGISTRY_ID,
-      OutcomeDetail.OutcomeId AS SKILL_DETAIL_ID,
-      OutcomeDetail.Description AS SKILL_DETAIL_DESCRIPTION,
-      OutcomeDetail.ParentOutcomeId AS SKILL_DETAIL_PARENT_ID,
-      'Course' AS SKILL_DETAIL_SOURCE
-    FROM
-      SKILL_DETAIL,
-      D2L_OUTCOME_DETAIL OutcomeDetail,
-      (
-        select
-          A.OutcomeId as OutcomeId,
-          min(A.RegistryId) as RegistryId
-        from
-          D2L_OUTCOMES_IN_REGISTRY A
-        where
-          A.CreatedDate = (
-            select
-              min(B.CreatedDate)
-            from
-              D2L_OUTCOMES_IN_REGISTRY B
-            where
-              B.OutcomeId = A.OutcomeId and
-              NVL(B.IsDeleted, 0) = 0
-          ) AND
-          A.LastModifiedDate = (
-            select
-              min(B.LastModifiedDate)
-            from
-              D2L_OUTCOMES_IN_REGISTRY B
-            where
-              B.OutcomeId = A.OutcomeId AND
-              B.CreatedDate = A.CreatedDate and
-              NVL(B.IsDeleted, 0) = 0
-          ) AND
-           NVL(A.IsDeleted, 0) = 0
-        group by
-          A.OutcomeId
-        having
-          count(1) = 1
-      ) OutcomeRegistry
-    WHERE
-      OutcomeDetail.ParentOutcomeId = SKILL_DETAIL_PARENT_ID AND
-      NVL(OutcomeDetail.IsDeleted, 0) = 0 AND
-      not exists (
-        select
-          1
-        from
-          SKILL_DETAIL i
+        SELECT DISTINCT
+          Parent.SKILL_DETAIL_PROGRAM_ID,
+          Parent.SKILL_DETAIL_PROGRAM_NAME,
+          Child.SKILL_DETAIL_OWNER_ID,
+          Child.SKILL_DETAIL_ID
+        FROM
+          SKILL_DETAIL Parent,
+          SKILL_DETAIL Child
         WHERE
-          i.SKILL_DETAIL_ID = OutcomeDetail.OutcomeId
-      ) and
-      OutcomeRegistry.OutcomeId = OutcomeDetail.OutcomeId
+          Parent.SKILL_DETAIL_PROGRAM_ID IS NOT NULL AND
+          Child.SKILL_DETAIL_PARENT_ID = Parent.SKILL_DETAIL_ID AND
+          Child.SKILL_DETAIL_PROGRAM_ID IS NULL
+      ) B
+    ON
+      (
+        A.SKILL_DETAIL_OWNER_ID = B.SKILL_DETAIL_OWNER_ID AND
+        A.SKILL_DETAIL_ID = B.SKILL_DETAIL_ID
+      )
+    WHEN MATCHED THEN
+      UPDATE SET
+        A.SKILL_DETAIL_PROGRAM_ID = B.SKILL_DETAIL_PROGRAM_ID,
+        A.SKILL_DETAIL_PROGRAM_NAME = B.SKILL_DETAIL_PROGRAM_NAME
     ;
 
     SELECT
@@ -105,52 +103,12 @@ BEGIN
     INTO
       record_count
     FROM
-      SKILL_DETAIL,
-      D2L_OUTCOME_DETAIL OutcomeDetail,
-      (
-        select
-          A.OutcomeId as OutcomeId,
-          min(A.RegistryId) as RegistryId
-        from
-          D2L_OUTCOMES_IN_REGISTRY A
-        where
-          A.CreatedDate = (
-            select
-              min(B.CreatedDate)
-            from
-              D2L_OUTCOMES_IN_REGISTRY B
-            where
-              B.OutcomeId = A.OutcomeId and
-              NVL(B.IsDeleted, 0) = 0
-          ) AND
-          A.LastModifiedDate = (
-            select
-              min(B.LastModifiedDate)
-            from
-              D2L_OUTCOMES_IN_REGISTRY B
-            where
-              B.OutcomeId = A.OutcomeId AND
-              B.CreatedDate = A.CreatedDate and
-              NVL(B.IsDeleted, 0) = 0
-          ) AND
-          NVL(A.IsDeleted, 0) = 0
-        group by
-          A.OutcomeId
-        having
-          count(1) = 1
-      ) OutcomeRegistry
+      SKILL_DETAIL Parent,
+      SKILL_DETAIL Child
     WHERE
-      OutcomeDetail.ParentOutcomeId = SKILL_DETAIL_PARENT_ID AND
-      NVL(OutcomeDetail.IsDeleted, 0) = 0 AND
-      not exists (
-        select
-          1
-        from
-          SKILL_DETAIL i
-        WHERE
-          i.SKILL_DETAIL_ID = OutcomeDetail.OutcomeId
-      ) and
-      OutcomeRegistry.OutcomeId = OutcomeDetail.OutcomeId
+      Parent.SKILL_DETAIL_PROGRAM_ID IS NOT NULL AND
+      Child.SKILL_DETAIL_PARENT_ID = Parent.SKILL_DETAIL_ID AND
+      Child.SKILL_DETAIL_PROGRAM_ID IS NULL
     ;
 
     EXIT WHEN record_count < 1;
